983df115a23e425f3a07ed94c3fc150af36daf3e
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-06-11 18:52:37 +0100

    Improve prompt for entering coordinates of point 2

--- C/Calcul_distance_euclidienne_entre_2_points.c
+++ C/Calcul_distance_euclidienne_entre_2_points.c
@@ -14,7 +14,7 @@ int main() {
     printf("Saisir les coordonnees du point 1 (x y) : ");
     scanf("%f %f", &p1.x, &p1.y);
 
-    printf("Saisir coordonnees du point 2 (x y) : ");
+    printf("Saisir les coordonnees du point 2 (x y) : ");
     scanf("%f %f", &p2.x, &p2.y);
 
     distance = sqrt((p2.x - p1.x) * (p2.x - p1.x)
